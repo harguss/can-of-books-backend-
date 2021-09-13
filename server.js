a3285cbc330aa4db980da7f15b216ea97cc809c7
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-13 16:03:45 -0500

    install and connect with mongo

--- server.js
+++ server.js
@@ -3,10 +3,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to mongo!');
+})
+mongoose.connect(process.env.MONGODB_URL);
 
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
